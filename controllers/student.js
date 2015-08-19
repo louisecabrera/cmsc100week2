17afc12e5ae097e4fc082951f50ac0c80e8313c0
AleliLegaspi <aslegaspi@up.edu.ph>
2015-08-19 11:33:51 +0800

    week2

---
+++ controllers/student.js
@@ -0,0 +1,4 @@
+exports.find = function(req,res){
+
+      res.send('Hello World!');
+};
---
+++ controllers/teacher.js
@@ -0,0 +1,9 @@
+exports.find = function(req,res){
+
+      res.send('Im a teacher');
+};
+
+exports.add = function(req,res){
+
+      res.send('Hello!');
+};
