a8c9215a9dec70ecd17aa540411fd278b87cdc86
louisecabrera <kbcabrera1@up.edu.ph>
2015-08-26 12:24:51 +0800

    crud

--- config/router.js
+++ config/router.js
@@ -1,7 +1,5 @@
 var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
-
-
 /*module.exports = function(router){ //export functions on other modules
       router.route('/students')
             .get(student.find);  //callback function is transferred to controllers.student
@@ -11,11 +9,16 @@ var teacher = require('./../controllers/teacher');
       return router;
 };*/
 module.exports = function(router){ //export functions on other modules
-      router.route('/students')
-            .get(student.find);  //callback function is transferred to controllers.student
-      router.route('/teachers')
+      router.route('/students') //localhost:5000/students 
+            .get(student.find)  //callback function is transferred to controllers.student
+            .post (student.insert);//insert function in student.js
+     router.route('/students/:id') //retrieve 1 data (id), ask id parameter
+            .get(student.findOne) //findOne function in student.js
+            .put(student.update)//update function in student.js
+            .delete(student.remove);//delete function in student.js
+     router.route('/teachers')//localhost:5000/teachers
             .get(teacher.find)
-            .post(teacher.add);
-       
+            .post(teacher.add); // ';' for the last method in the statement
+      
       return router;
 };
--- controllers/student.js
+++ controllers/student.js
@@ -1,4 +1,56 @@
-exports.find = function(req,res){
+var db = require (__dirname+ '/../lib/mysql') //connect 
+
+/*exports.find = function(req,res){
 
       res.send('Hello World!');
-};
+};*/
+exports.find = function(req,res,next){
+
+      db.query("SELECT * FROM student", function(err,rows){ //2 parameters (sql statement, function to implement)
+            if(err) return next (err); //if error, skip route handlers
+            //else, send rows retrieved from query
+            res.send(rows);
+      });
+      
+   };
+exports.findOne = function(req,res,next){
+
+      db.query("SELECT * FROM student WHERE id=?",[req.params.id], function(err,rows){
+            if(err) return next (err);
+            if(rows.length===0){ //=== same type
+                  res.status(404).send('student not found');
+            } else{
+            res.send(rows[0]);
+            }
+      });
+      
+   };
+   
+exports.insert = function(req,res,next){
+
+      db.query("INSERT INTO student(studno,name) VALUES (?,?)", [req.body.studno,req.body.name], function(err,rows){
+            if(err) return next (err);
+            res.send(rows);
+            
+      });
+      
+   };
+
+exports.update = function(req,res,next){
+
+      db.query("UPDATE student SET ? WHERE id=?", [req.body,req.params.id], function(err,rows){
+            if(err) return next (err);
+            res.send(rows);
+            
+      });
+      
+   };
+exports.remove = function(req,res,next){
+
+      db.query("DELETE FROM student WHERE id=?", [req.params.id], function(err,rows){
+            if(err) return next (err);
+            res.send(rows);
+            
+      });
+      
+   };
